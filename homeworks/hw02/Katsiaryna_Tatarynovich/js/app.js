58caec9015d16f11b3e80e597fdf78854f4da11d
Katsiaryna Tatarynovich <Katsiaryna_Tatarynovich@epam.com>
2017-09-27 15:27:20 +0300

    task02\fix: added overflow

--- homeworks/hw02/Katsiaryna_Tatarynovich/js/app.js
+++ homeworks/hw02/Katsiaryna_Tatarynovich/js/app.js
@@ -1,14 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const body = document.body;
     const btnShowMoon = document.getElementById('btnShowMoon');
     const btnCloseMoon = document.getElementById('btnCloseMoon');
     const moonElement = document.getElementById('moonElement');
 
     function showMoonElement() {
         moonElement.style.display = 'block';
+        body.style.overflow = 'hidden';
     }
 
     function hideMoonElement() {
         moonElement.style.display = 'none';
+        body.style.overflow = 'visible';
     }
 
     window.addEventListener('click', function(event) {
